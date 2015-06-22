1b89562aecf6a89b80e7732d56442e03acef7d4c
gaku <kuga729@gmail.com>
2015-06-22 23:20:07 +0900

    Initial Commit.

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+  "net/http"
+  "encoding/json"
+  "strings"
+  )
+
+type Temp struct {
+  Kelvin float64 `json:"temp"`
+}
+
+type weatherData struct {
+  Name string `json:"name"`
+  Temp `json:"main"`
+}
+
+func query(city string) (weatherData, error) {
+  resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city)
+  if err != nil {
+    return weatherData{}, err
+  }
+
+  defer resp.Body.Close()
+
+  var d weatherData
+
+  if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+    return weatherData{}, err
+  }
+
+  return d, nil
+}
+
+func main() {
+  http.HandleFunc("/", hello)
+  http.HandleFunc("/weather/", weather)
+  http.ListenAndServe(":8080", nil)
+}
+
+func weather(w http.ResponseWriter, r *http.Request){
+  city := strings.SplitN(r.URL.Path, "/", 3)[2]
+  data, err := query(city)
+
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json; charset=utf-8")
+  json.NewEncoder(w).Encode(data)
+}
+
+func hello(w http.ResponseWriter, r *http.Request){
+  w.Write([]byte("hello!!"))
+}
